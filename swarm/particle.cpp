f883ae2cb05dba99b3f577aea20e455b0efb63b5
Ben Stott <bgbnbigben@gmail.com>
2014-09-22 01:28:11 +1000

    General fuckery. None of this file will compile in this state.

--- swarm/particle.cpp
+++ swarm/particle.cpp
@@ -17,29 +17,32 @@ namespace {
     }
 }
 
-Particle::Particle(const std::vector<Bound<double>>& bounds, int dim) : 
-        bounds_(bounds) {
-    assert(bounds.size() == dim);
-    this->position_.resize(dim);
+template <typename T>
+Particle::Particle(const std::vector<VariableContainer>& position, int dim) : 
+        position_(position) {
+    assert(position.size() == dim);
     this->velocity_.resize(dim);
     this->bestPosition_.resize(dim);
+    //# pragma omp parallel for
+    //    for (int i = 0; i < dim; i++)
+    //        this->position_[i] = this->getRandomInWindow_(i);
     # pragma omp parallel for
-        for (int i = 0; i < dim; i++)
-            this->position_[i] = this->getRandomInWindow_(i);
-    # pragma omp parallel for
-        for (int i = 0; i < dim; i++)
-            this->velocity_[i] = this->getRandomInWindow_(i);
+        for (int i = 0; i < dim; i++) {
+            auto r = this->getRandomInWindow_(i);
+            this->velocity_[i] = this->position_[i].getType == VariableType::Continuous ? r.d : r.ll;
+            this->velocity_[i].setType(this->position_[i].getType());
+        }
     this->bestPosition_ = this->position_;
-    this->cost_ = 1e100;
+    this->cost_ = (T)1e100;
     this->clamp();
 }
 
-double& Particle::operator[] (int i) {
+Variable& Particle::operator[] (int i) {
     return this->position_[i];
 }
 
 /* TODO: make it clamp to window or re-init or something */
-void Particle::step(const std::vector<double>& direction, double c1, double c2, double momentum) {
+void Particle::step(const std::vector<Variable>& direction, double c1, double c2, double momentum) {
     # pragma omp single
     {
         this->velocity_ = momentum * (this->velocity_ +
@@ -53,25 +56,26 @@ void Particle::step(const std::vector<double>& direction, double c1, double c2, 
 void Particle::clamp() {
     # pragma omp parallel
     {
-        std::for_each(this->bounds_.begin(), this->bounds_.end(),
-            [&] (Bound<double> bound) {
-                if (bound.type == Bound<double>::types::LOWER ||
-                    bound.type == Bound<double>::types::BOTH) 
-                    this->position_[bound.variable] = std::max(this->position_[bound.variable], bound.lower);
-                if (bound.type == Bound<double>::types::UPPER ||
-                    bound.type == Bound<double>::types::BOTH)
-                    this->position_[bound.variable] = std::min(this->position_[bound.variable], bound.upper);
+        std::for_each(this->position_.begin(), this->position_.end(),
+            [&] (VariableContainer& pos) {
+                    // TODO make sure it's not fucking up and calling the wrong
+                    // cast operator. It probably always uses double.
+                    pos.val(std::max(pos.rangeStart, pos));
+                    pos.val(std::min(pos.rangeEnd, pos));
             });
     }
 }
 
-void Particle::reload(const std::vector<double>& p, const std::vector<double>& v) {
+void Particle::reload(const std::vector<Variable>& p, const std::vector<Variable>& v) {
     this->velocity_ = v;
     this->position_ = p;
     this->clamp();
 }
 
-double Particle::getRandomInWindow_(int i) {
-    return ::getRandom() * (this->bounds_[i].upper - this->bounds_[i].lower)
-        + this->bounds_[i].lower;
+VariableType Particle::getRandomInWindow_(int i) {
+    if (this->position_[i].getType == VariableType::Continuous)
+        return ::getRandom() * (this->position_[i].rangeEnd.d - this->position_[i].rangeStart.d)
+            + this->position_[i].rangeStart.d;
+        return ::getRandom() * (this->position_[i].rangeEnd.ll - this->position_[i].rangeStart.ll)
+            + this->position_[i].rangeStart.ll;
 }
